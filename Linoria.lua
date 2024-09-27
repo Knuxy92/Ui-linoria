8c0f185c588e042b13fee82fc083846575063588
Lightnine <90461595+Knuxy92@users.noreply.github.com>
2024-09-27 19:15:01 +0700

    Update Linoria.lua

--- Linoria.lua
+++ Linoria.lua
@@ -1478,7 +1478,7 @@ do
 			TextLabel.Text = Text;
 
 			if DoesWrap then
-				local Y = select(2, Library:GetTextBounds(Text, Library.Font, 14, Vector2.new(TextLabel.AbsoluteSize.X, math.huge)));
+				local Y = select(2, Library:GetTextBounds(Text, Library.Font, 16, Vector2.new(TextLabel.AbsoluteSize.X, math.huge)));
 				TextLabel.Size = UDim2.new(1, -4, 0, Y);
 			end;
 
